1b34b1083c6a6a2f0a444ce8a345a577d0626f6e
Mummoth <piotrpiwonibiz@gmail.com>
2022-03-18 10:45:05 +0000

    Updating Namespaces

---
+++ Assets/_Root/Scripts/Manager/CameraManager.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+namespace CultureFMP.Manager
+{
+    public class CameraManager : MonoBehaviour
+    {
+        private InputManager inputManager;
+        private Transform cameraTransform;
+        private Vector3 cameraFollowVelocity = Vector3.zero;
+        private Vector3 cameraVectorPosition;
+        private float defualtPosition;
+
+        public LayerMask collisionLayers;
+        public Transform targetTransform;
+        public Transform cameraPivot;
+
+        public float cameraFollowSpeed = 0.2f;
+        public float cameraLookSpeed = 2;
+        public float cameraPivotSpeed = 2;
+        public float minimumPivotAngle = -35;
+        public float maximumPivotAngle = 35;
+        public float lookAngle;
+        public float pivotAngle;
+        public float cameraCollisionRadius = 2;
+        public float cameraCollisionOffset = 0.2f;
+        public float minimumCollisionOffset = 0.2f;
+
+        private void Awake()
+        {
+            targetTransform = FindObjectOfType<PlayerManager>().transform;
+            inputManager = FindObjectOfType<InputManager>();
+            cameraTransform = Camera.main.transform;
+            defualtPosition = cameraTransform.localPosition.z;
+        }
+
+        public void HandleAllCameraMovement()
+        {
+            FollowTarget();
+            RotateCamera();
+            HandleCameraCollision();
+        }
+
+        private void FollowTarget()
+        {
+            Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed);
+
+            transform.position = targetPosition;
+        }
+
+        private void RotateCamera()
+        {
+            Vector3 rotation;
+            Quaternion targetRotation;
+
+            lookAngle += (inputManager.cameraInputX * cameraLookSpeed);
+            pivotAngle += (inputManager.cameraInputY * cameraPivotSpeed);
+            pivotAngle = Mathf.Clamp(pivotAngle, minimumPivotAngle, maximumPivotAngle);
+
+            rotation = Vector3.zero;
+            rotation.y = lookAngle;
+            targetRotation = Quaternion.Euler(rotation);
+            transform.rotation = targetRotation;
+
+            rotation = Vector3.zero;
+            rotation.x = pivotAngle;
+            targetRotation = Quaternion.Euler(rotation);
+            cameraPivot.localRotation = targetRotation;
+        }
+
+        private void HandleCameraCollision()
+        {
+            float targetPosition = defualtPosition;
+            RaycastHit hit;
+
+            Vector3 direction = cameraTransform.position - cameraPivot.position;
+            direction.Normalize();
+
+            if (Physics.SphereCast(cameraPivot.transform.position, cameraCollisionRadius, direction, out hit, Mathf.Abs(targetPosition), collisionLayers))
+            {
+                float distance = Vector3.Distance(cameraPivot.position, hit.point);
+                targetPosition -= (distance - cameraCollisionOffset);
+            }
+
+            if (Mathf.Abs(targetPosition) < minimumCollisionOffset)
+            {
+                targetPosition -= minimumCollisionOffset;
+            }
+
+            cameraVectorPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, 0.2f);
+            cameraTransform.localPosition = cameraVectorPosition;
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/Manager/PlayerManager.cs
@@ -0,0 +1,31 @@
+using CultureFMP.Manager;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    InputManager inputManager;
+    CharacterLocomotion characterLocomotion;
+    CameraManager cameraManager;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+        characterLocomotion = GetComponent<CharacterLocomotion>();
+        cameraManager = FindObjectOfType<CameraManager>();
+    }
+
+    private void Update()
+    {
+        inputManager.HundalAllInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        characterLocomotion.HandleAllMovement();    
+    }
+
+    private void LateUpdate()
+    {
+        cameraManager.HandleAllCameraMovement();
+    }
+}
