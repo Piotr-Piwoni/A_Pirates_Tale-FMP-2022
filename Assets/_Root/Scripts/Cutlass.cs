46c7ebbeaa67c8d6836641cf81e38662fd1265ab
Viktor Abagian <vabagian@gmail.com>
2022-03-18 10:31:15 +0000

    Update Cutlass.cs
    + fetched collider

--- Assets/_Root/Scripts/Cutlass.cs
+++ Assets/_Root/Scripts/Cutlass.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Cutlass : MonoBehaviour
 {
+    Collider myCol;
+
+    private void Start()
+    {
+        myCol = gameObject.GetComponent<Collider>();
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             // Here the animation for the cutlass swing will go
+            // Once the animation is implemted I will also toggle the collider on during the animation and off after it to avoid any collsion problems using Collider.enable
         }
     }
 
