744de1deb1d786698e15f7069bdd2edd7f50c95e
0ArcDrive0 <piotrpiwonibiz@gmail.com>
2022-03-23 10:33:42 +0000

    Basic Forward Ship Motion

--- Assets/_Root/Scripts/Movement/ShipMovement.cs
+++ Assets/_Root/Scripts/Movement/ShipMovement.cs
@@ -8,19 +8,13 @@ namespace CultureFMP.Movement
         public float shipSpeed = 2;
         public bool isMoving;
 
-        private Vector3 _shipPos;
-
-        private void Awake()
-        {
-            _shipPos = transform.position;
-        }
 
         private void Update()
         {
             if (isMoving)
             {
-                _shipPos = Vector3.forward * 1000f; //* shipSpeed * Time.deltaTime
-            }
+                transform.position += Vector3.forward * shipSpeed * Time.deltaTime;
+            }            
         }
 
     }
