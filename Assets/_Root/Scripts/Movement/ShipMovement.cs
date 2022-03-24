266eb9ae8655ce77e4736e11ac03009b1db78c14
Viktor Abagian <vabagian@gmail.com>
2022-03-24 10:50:24 +0000

    Started adding ship sway
    + added ship rotation randomiser
    - its still broken

--- Assets/_Root/Scripts/Movement/ShipMovement.cs
+++ Assets/_Root/Scripts/Movement/ShipMovement.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace CultureFMP.Movement
 {
@@ -8,14 +9,33 @@ namespace CultureFMP.Movement
         public float shipSpeed = 2;
         public bool isMoving;
 
+        private Quaternion shipSway;
+
+        private void Start()
+        {
+            shipSway = transform.rotation;
+        }
 
         private void Update()
         {
             if (isMoving)
             {
                 transform.position += Vector3.forward * shipSpeed * Time.deltaTime;
-            }            
+            }
+
+            if (transform.rotation == shipSway)
+            {
+                RandomSway();
+            }
+
+            //transform.rotation = Quaternion.Lerp(transform.rotation, shipSway, Time.deltaTime * 0.1f);
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, shipSway, Time.deltaTime);
         }
 
+        private void RandomSway()
+        {
+            shipSway.Set(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), 0);
+            Debug.Log($"{shipSway.x} {shipSway.y} {shipSway.z}");
+        }
     }
 }
