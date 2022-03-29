948e5c092f24ca46ca1b07251f4160e581765e9f
Viktor Abagian <vabagian@gmail.com>
2022-03-29 11:50:12 +0100

    Ship Sway progress
    + Progress made on ship sway

--- Assets/_Root/Scripts/Movement/ShipMovement.cs
+++ Assets/_Root/Scripts/Movement/ShipMovement.cs
@@ -11,9 +11,12 @@ namespace CultureFMP.Movement
 
         private Quaternion _shipSway;
 
+        float xTilt = 5f;
+        float zTilt = 10f;
+
         private void Start()
         {
-            _shipSway = transform.rotation;
+            //_shipSway = transform.rotation;
         }
 
         private void Update()
@@ -23,29 +26,34 @@ namespace CultureFMP.Movement
                 transform.position += Vector3.forward * shipSpeed * Time.deltaTime;
             }
 
-            if (transform.rotation == _shipSway)
+
+
+            transform.eulerAngles = new Vector3(Mathf.Sin(Time.time * xTilt) * 10.0f, transform.eulerAngles.y, Mathf.Sin(Time.time * zTilt) * 10.0f);
+
+            /*if (transform.rotation == _shipSway)
             {
                 RandomSway();
-            }
+            }*/
 
             //transform.rotation = Quaternion.Lerp(transform.rotation, _shipSway, Time.deltaTime * 0.1f);
             //transform.rotation = Quaternion.Slerp(transform.rotation, shipSway, Time.deltaTime * 0.1f);
             //transform.rotation = Quaternion.RotateTowards(transform.rotation, shipSway, Time.deltaTime);
-            //transform.Rotate(_shipSway.x, _shipSway.y, _shipSway.z, Space.World); Space.Self doesnt work either
+            //transform.Rotate(_shipSway.x, _shipSway.y, _shipSway.z, Space.Self);
             //transform.localRotation = Quaternion.Lerp(transform.rotation, _shipSway, Time.deltaTime * 0.1f);
 
-            /*            if (Input.GetKeyDown("j"))
-                        {
-                            _shipSway.Set(5.0f, 0f, 5.0f, 0);
-                        }*/
+           /*if (Input.GetKeyDown("j"))
+           {
+               //_shipSway.Set(5.0f, 0f, 5.0f, 0);
+               RandomSway();
+           }*/
 
 
         }
 
         private void RandomSway()
         {
-            _shipSway.Set(Random.Range(-5.0f, 5.0f), 0.0f, Random.Range(-5.0f, 5.0f), 0);
-            Debug.Log($"{_shipSway.x} {_shipSway.y} {_shipSway.z}");
+            /*_shipSway.Set(Random.Range(-5.0f, 5.0f), 0.0f, Random.Range(-5.0f, 5.0f), 0);
+            Debug.Log($"{_shipSway.x} {_shipSway.y} {_shipSway.z}");*/
             //transform.Rotate(_shipSway.x, _shipSway.y, _shipSway.z, Space.Self);
         }
     }
