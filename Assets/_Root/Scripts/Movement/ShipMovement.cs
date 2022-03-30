49e7dbfbf4afcd1a086dcfcfea62ca59856326bd
Viktor Abagian <vabagian@gmail.com>
2022-03-30 10:05:07 +0100

    Update ShipMovement.cs
    + added sway settings to the inspector and set the default settings to look pretty decent

--- Assets/_Root/Scripts/Movement/ShipMovement.cs
+++ Assets/_Root/Scripts/Movement/ShipMovement.cs
@@ -9,15 +9,11 @@ namespace CultureFMP.Movement
         public float shipSpeed = 2;
         public bool isMoving;
 
-        private Quaternion _shipSway;
+        [Header ("Sway Settings")]
 
-        float xTilt = 5f;
-        float zTilt = 10f;
-
-        private void Start()
-        {
-            //_shipSway = transform.rotation;
-        }
+        [SerializeField] [Tooltip ("This changes how fast it tilts on the x Axis")] private float xTilt = 0.5f;
+        [SerializeField] [Tooltip ("This changes how fast it tilts on the z Axis")] private float zTilt = 1.0f;
+        [SerializeField] [Tooltip ("This changes how far it sways")] private float sway = 3.0f;
 
         private void Update()
         {
@@ -26,35 +22,7 @@ namespace CultureFMP.Movement
                 transform.position += Vector3.forward * shipSpeed * Time.deltaTime;
             }
 
-
-
-            transform.eulerAngles = new Vector3(Mathf.Sin(Time.time * xTilt) * 10.0f, transform.eulerAngles.y, Mathf.Sin(Time.time * zTilt) * 10.0f);
-
-            /*if (transform.rotation == _shipSway)
-            {
-                RandomSway();
-            }*/
-
-            //transform.rotation = Quaternion.Lerp(transform.rotation, _shipSway, Time.deltaTime * 0.1f);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, shipSway, Time.deltaTime * 0.1f);
-            //transform.rotation = Quaternion.RotateTowards(transform.rotation, shipSway, Time.deltaTime);
-            //transform.Rotate(_shipSway.x, _shipSway.y, _shipSway.z, Space.Self);
-            //transform.localRotation = Quaternion.Lerp(transform.rotation, _shipSway, Time.deltaTime * 0.1f);
-
-           /*if (Input.GetKeyDown("j"))
-           {
-               //_shipSway.Set(5.0f, 0f, 5.0f, 0);
-               RandomSway();
-           }*/
-
-
-        }
-
-        private void RandomSway()
-        {
-            /*_shipSway.Set(Random.Range(-5.0f, 5.0f), 0.0f, Random.Range(-5.0f, 5.0f), 0);
-            Debug.Log($"{_shipSway.x} {_shipSway.y} {_shipSway.z}");*/
-            //transform.Rotate(_shipSway.x, _shipSway.y, _shipSway.z, Space.Self);
+            transform.eulerAngles = new Vector3(Mathf.Sin(Time.time * xTilt) * sway, transform.eulerAngles.y, Mathf.Sin(Time.time * zTilt) * sway);
         }
     }
 }
