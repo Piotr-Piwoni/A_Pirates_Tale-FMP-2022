98edce5fd577aea1023a245daa4f58c3cd9b4e46
Mummoth <piotrpiwonibiz@gmail.com>
2022-04-16 16:49:59 +0100

    Setup Animation Manager
    + Created an Animation Manger
    + Created a Public Method Called UpdateAnimatorValues()
    + Calling the Method in Input Manager
    + Setup a Bland Tree for the Three Basic Animations; Idle, Walking and Running

--- Assets/_Root/Scripts/Manager/AnimatorManager.cs
+++ Assets/_Root/Scripts/Manager/AnimatorManager.cs
@@ -1,21 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-namespace CultureFMP
+namespace CultureFMP.Manager
 {
     public class AnimatorManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private Animator _animator;
+        private int _horizontal;
+        private int _vertical;
+
+        private void Awake()
         {
-        
+            _animator = GetComponent<Animator>();
+            _horizontal = Animator.StringToHash("Horizontal");
+            _vertical = Animator.StringToHash("Vertical");
         }
 
-        // Update is called once per frame
-        void Update()
+        public void UpdateAnimatorValues(float _horizontalMovement, float _verticalMovement)
         {
-        
+            float _snappedHorizontal;
+            float _snappedVertical;
+
+            #region Snapped Horizontal
+
+                if (_horizontalMovement > 0 && _horizontalMovement < 0.55f)
+                {
+                    _snappedHorizontal = 0.5f;
+                } else if (_horizontalMovement > 0.55f)
+                {
+                    _snappedHorizontal = 1;
+                } else if (_horizontalMovement < 0 && _horizontalMovement > -0.55f)
+                {
+                    _snappedHorizontal = -0.5f;
+                } else if (_horizontalMovement < -0.55f)
+                {
+                    _snappedHorizontal = -1;
+                } else
+                {
+                    _snappedHorizontal = 0;
+                }
+                
+            #endregion
+            #region Snapped Vertical
+            
+                if (_verticalMovement > 0 && _verticalMovement < 0.55f)
+                {
+                    _snappedVertical = 0.5f;
+                } else if (_verticalMovement > 0.55f)
+                {
+                    _snappedVertical = 1;
+                } else if (_verticalMovement < 0 && _verticalMovement > -0.55f)
+                {
+                    _snappedVertical = -0.5f;
+                } else if (_verticalMovement < -0.55f)
+                {
+                    _snappedVertical = -1;
+                } else
+                {
+                    _snappedVertical = 0;
+                }
+
+            #endregion
+            
+            _animator.SetFloat(_horizontal, _snappedHorizontal, 0.1f, Time.deltaTime);
+            _animator.SetFloat(_vertical, _snappedVertical, 0.1f, Time.deltaTime);
         }
     }
 }
--- Assets/_Root/Scripts/Manager/InputManager.cs
+++ Assets/_Root/Scripts/Manager/InputManager.cs
@@ -8,10 +8,12 @@ namespace CultureFMP.Manager
     {
         private InputActions _inputActions;
         private CharacterLocomotion _characterLocomotion;
+        private AnimatorManager _animatorManager;
 
         [Header("Debugging Stats")]
         [SerializeField] private Vector2 movementInput;
         [SerializeField] private Vector2 cameraInput;
+        [SerializeField] private float moveAmount;
         [SerializeField] private bool jumpInput;
 
         public float cameraInputX;
@@ -21,7 +23,8 @@ namespace CultureFMP.Manager
 
         private void Awake()
         {
-            _characterLocomotion = GetComponent<CharacterLocomotion>();    
+            _characterLocomotion = GetComponent<CharacterLocomotion>();
+            _animatorManager = GetComponent<AnimatorManager>();
         }
 
         private void OnEnable()
@@ -55,6 +58,9 @@ namespace CultureFMP.Manager
             verticalInput = movementInput.y;
             horizontalInput = movementInput.x;
 
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+            _animatorManager.UpdateAnimatorValues(0,moveAmount);
+            
             cameraInputY = cameraInput.y;
             cameraInputX = cameraInput.x;
         }
