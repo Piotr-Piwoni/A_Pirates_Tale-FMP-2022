4466448a5946331a13436b55ac4158eb4452b996
Mummoth <piotrpiwonibiz@gmail.com>
2022-04-18 19:22:20 +0100

    Update ResetBool.cs

--- Assets/_Root/Scripts/Animator Behaviour/ResetBool.cs
+++ Assets/_Root/Scripts/Animator Behaviour/ResetBool.cs
@@ -10,7 +10,7 @@ namespace CultureFMP.AnimatorBehaviour
         //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            animator.SetBool(isInteractingBool, isInteractingStatus);    
+            animator.SetBool(isInteractingBool, isInteractingStatus);
         }
     }
 }
