0ee84b197f13fb3ad2b3dec42bd721f7abebb032
Mummoth <piotrpiwonibiz@gmail.com>
2022-05-22 20:06:04 +0100

    Made Player Manager get NPC Animator
    + Smooth transition between idle and talking animations for the Captain
    + Moved the NPC Animator assigning to the Player Manager instead

--- Assets/_Root/Scripts/Manager/AnimatorManager.cs
+++ Assets/_Root/Scripts/Manager/AnimatorManager.cs
@@ -8,7 +8,6 @@ namespace CultureFMP.Manager
     {
         [HideInInspector]
         public Animator animator;
-        public Animator npcAnimator;
         public PlayerManager playerManager;
         
         private int _horizontal;
@@ -27,9 +26,9 @@ namespace CultureFMP.Manager
             animator.CrossFade(_targetAnimation, 0.2f);
         }
 
-        public void PlayAnimationInDialogue(bool _inDialogue)
+        public void PlayAnimationInDialogue(Animator _animator , bool _inDialogue)
         {
-            npcAnimator.SetBool("InDialogue", _inDialogue);
+            _animator.SetBool("InDialogue", _inDialogue);
         }
 
         public void UpdateAnimatorValues(float _horizontalMovement, float _verticalMovement, bool _isSprinting)
@@ -92,14 +91,6 @@ namespace CultureFMP.Manager
             
             animator.SetFloat(_horizontal, _snappedHorizontal, 0.1f, Time.deltaTime);
             animator.SetFloat(_vertical, _snappedVertical, 0.1f, Time.deltaTime);
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.GetComponent<Animator>() != null)
-            {
-                npcAnimator = other.GetComponent<Animator>();
-            }
         }
     }
 }
--- Assets/_Root/Scripts/Manager/PlayerManager.cs
+++ Assets/_Root/Scripts/Manager/PlayerManager.cs
@@ -9,6 +9,7 @@ namespace CultureFMP.Manager
     public class PlayerManager : MonoBehaviour
     {
         private Animator _animator;
+        public Animator npcAnimator = null;
         private InputManager _inputManager;
         private CharacterLocomotion _characterLocomotion;
         private CameraManager _cameraManager;
@@ -26,6 +27,7 @@ namespace CultureFMP.Manager
             _characterLocomotion = GetComponent<CharacterLocomotion>();
             _cameraManager = FindObjectOfType<CameraManager>();
             _animatorManager = GetComponent<AnimatorManager>();
+            npcAnimator = _animator;
         }
 
         private void Update()
@@ -35,18 +37,16 @@ namespace CultureFMP.Manager
             if (VD.isActive)
             {
                 inDialogue = true;
-                _animatorManager.PlayAnimationInDialogue(inDialogue);
+                _animatorManager.PlayAnimationInDialogue(npcAnimator, inDialogue);
             }
             else
             {
                 inDialogue = false;
-                _animatorManager.PlayAnimationInDialogue(inDialogue);
+                _animatorManager.PlayAnimationInDialogue(npcAnimator, inDialogue);
             }
             
             if (Input.GetKeyDown(KeyCode.E))
-            {
                 TryInteract();
-            }
         }
 
         
@@ -67,9 +67,10 @@ namespace CultureFMP.Manager
         private void OnTriggerEnter(Collider _other)
         {
             if (_other.GetComponent<VIDE_Assign>() != null)
-            {
                 _currentDialogue = _other.GetComponent<VIDE_Assign>();
-            }
+            
+            if (_other.GetComponent<Animator>() != null)
+                npcAnimator = _other.GetComponent<Animator>();
         }
 
         private void OnTriggerExit(Collider _other)
