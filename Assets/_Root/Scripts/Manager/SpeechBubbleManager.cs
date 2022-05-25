56c444004da33b39570ec19f9f3a66b39b09cd13
Viktor Abagian <vabagian@gmail.com>
2022-05-25 11:58:27 +0100

    SpeechThings
    + added speech bubbles

---
+++ Assets/_Root/Scripts/Manager/SpeechBubbleManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace CultureFMP
+{
+    public class SpeechBubbleManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _yelowBubble;
+        [SerializeField] private GameObject _whiteBubble;
+        private Camera cam;
+
+        private void Start()
+        {
+            cam = Camera.main;
+        }
+
+        public void Update()
+        {
+            transform.LookAt(cam.transform);
+        }
+
+        public void YellowToWhite()
+        {
+            _yelowBubble.SetActive(true);
+            _whiteBubble.SetActive(false);
+        }
+
+        public void WhiteToYellow()
+        {
+            _yelowBubble.SetActive(false);
+            _whiteBubble.SetActive(true);
+        }
+
+        public void NoBubbles()
+        {
+            _yelowBubble.SetActive(false);
+            _whiteBubble.SetActive(false);
+        }
+    }
+}
